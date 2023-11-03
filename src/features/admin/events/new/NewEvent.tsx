f56e343920dffd945e42558f9be64a0c47c3c395
fukuhara.yusuke <fyusuke.biz@gmail.com>
2023-11-03 23:27:01 +0900

    sve

--- src/features/admin/events/new/NewEvent.tsx
+++ src/features/admin/events/new/NewEvent.tsx
@@ -2,6 +2,7 @@ import { Box, Button, Container } from "@mui/material";
 import { NewEventFormProvider } from "./NewEventFormProvider";
 import { NewEventForm } from "./NewEeventForm";
 import { SubPageHeader } from "@/components/layouts/SubPageHeader";
+import Link from "next/link";
 
 export const NewEvent = () => {
   return (
@@ -17,9 +18,15 @@ export const NewEvent = () => {
       <SubPageHeader title="新規イベント作成" />
       <Container maxWidth="sm" sx={{ paddingY: "24px", background: "white" }}>
         <Box sx={{ display: "flex" }}>
-          <Button variant="outlined" sx={{ marginLeft: "auto" }}>
-            新規作成
-          </Button>
+          <Link
+            href="/admin/events/new"
+            style={{ textDecoration: "none" }}
+            passHref
+          >
+            <Button variant="outlined" sx={{ marginLeft: "auto" }}>
+              新規作成
+            </Button>
+          </Link>
         </Box>
         <NewEventFormProvider>
           <NewEventForm />
