780cf7c59bde337c2ce0a51c6823528099e266c8
fukuhara.yusuke <fyusuke.biz@gmail.com>
2023-11-18 22:16:21 +0900

    イメージ修正

--- src/pages/recruits/[id]/index.tsx
+++ src/pages/recruits/[id]/index.tsx
@@ -1,5 +1,60 @@
 import { Recruit } from "@/features/recruits/detail/Recruit";
+import { generateReadSignedUrl } from "@/libs/cloudStorage";
+import prisma from "@/libs/prisma";
+import { GetServerSideProps } from "next";
+import { NextSeo } from "next-seo";
 
-export default function RecruitPage() {
-  return <Recruit />;
+export const getServerSideProps: GetServerSideProps = async ({
+  req,
+  res,
+  query,
+}) => {
+  const recruitId = query.id as string;
+
+  const recruit = await prisma.recruit.findUniqueOrThrow({
+    where: { id: recruitId },
+    include: { eventLocation: { include: { event: true } } },
+  });
+
+  const coverImageFileUrl = recruit.eventLocation?.event.coverImageFileKey
+    ? await generateReadSignedUrl(recruit.eventLocation.event.coverImageFileKey)
+    : undefined;
+
+  const name = recruit.manualEventName
+    ? recruit.manualEventName
+    : recruit.eventLocation!.event.name;
+
+  console.log("recruit", recruit);
+  console.log("coverImageFileUrl", coverImageFileUrl);
+  console.log("name", name);
+
+  return { props: { event: { name, coverImageFileUrl } } };
+};
+
+type Props = {
+  event: {
+    name: string;
+    coverImageFileUrl?: string;
+  };
+};
+
+export default function RecruitPage({ event }: Props) {
+  return (
+    <>
+      <NextSeo
+        openGraph={{
+          title: event.name,
+          ...(event.coverImageFileUrl && {
+            images: [
+              {
+                url: event.coverImageFileUrl,
+                alt: event.name,
+              },
+            ],
+          }),
+        }}
+      />
+      <Recruit />
+    </>
+  );
 }
